07cafec882155f564c4cd39c0ed3bf87d3e7e7a7
ycqin <qinyc@hotmail.com>
2015-08-26 15:51:27 +0800

    update

---
+++ DOMScrptingExp/scripts/home.js
@@ -0,0 +1,5 @@
+var frame = document.createElement("img");
+frame.setAttribute("src","images/frame.gif");
+frame.setAttribute("alt","");
+frame.setAttribute("id","frame");
+slideshow.appendChild(frame);
--- qin.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('p').hover(function() {
-        $('p').css('font-size', '60px');
-    },
-    function(){
-    	$('p').css('font-size', '50px');
-    });
-});
